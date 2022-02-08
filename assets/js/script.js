5c00b81d71fc7b14e5f03162f9a292e4c3dd44b1
SadekRahman <sadekur99@hotmail.co.uk>
2022-02-08 22:31:12 +0000

    full structure of html added

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByName("button");
+    let alphabet = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+    ]
+})
