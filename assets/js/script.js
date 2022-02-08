70ac7ee4e1a70c74ba945585e6315081894bbaf8
SadekRahman <sadekur99@hotmail.co.uk>
2022-02-08 23:13:42 +0000

    set arrays for topics in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,42 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByName("button");
-    let alphabet = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
-    ]
-})
+const football = [
+    "Arsenal",
+    "Manchester United",
+    "Manchester City",
+    "Leicester",
+    "Chelsea",
+    "Tottenham",
+    "Aston Villa",
+    "Wolves",
+    "Liverpool",
+]
+const actors = [
+    "Dwayne Johnson",
+    "Robert Downey Jr",
+    "Chris Hemsworth",
+    "Chris Evans",
+    "Scarlett Johansen",
+    "Al Pacino",
+    "Cilian Murphy"
+]
+const capitals = [
+    "London",
+    "Madrid",
+    "Paris",
+    "Washington D.C",
+    "Moscow",
+    "Beijing",
+    "Mumbai",
+    "Dhaka",
+    "Berlin",
+]
+
+
+
+
+//create alphabet
+let alphabet = function () {
+    theAlphabet = document.getElementById("alphabet");
+    letters = document.createElement("ol");
+    for (let i=0; i < alphabet.length; i++);
+
+}
