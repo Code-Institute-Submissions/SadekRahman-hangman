6ac9675961693575b55eecdbabe241762093918c
SadekRahman <sadekur99@hotmail.co.uk>
2022-02-09 20:43:39 +0000

    keyboard added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 //variables from HTMl page
-const answer = "";
-const clueEl = document.getElementById("clue");
-const guesses = [];
-const maxIncorrectLetters = 10;
-const incorrectLetters = 0;
-const wordStatus = null;
+let answer = "";
+let clueEl = document.getElementById("clue");
+let guesses = [];
+let maxIncorrectLetters = 10;
+let incorrectLetters = 0;
+let wordStatus = null;
 
 //array of words
-const words = [
+let football_actor_capitals = [
     "Arsenal",
     "Manchester United",
     "Manchester City",
@@ -35,7 +35,7 @@ const words = [
     "Berlin"
 ]
 //array of corresponding clues
-const clues = [
+let clues = [
     "Football Team",
     "Football Team",
     "Football Team",
@@ -63,13 +63,24 @@ const clues = [
     "Capital City",
 ]
 //variable to track performance
-const correctLetters = [];
-const incorrectLetters = [];
+//const correctLetters = [];
 
 //choosing the random word
 function randomWord() {
-    answer = words[Math.floor(Math.random() * words.length)];
+    answer = football_actor_capitals[Math.floor(Math.random() * football_actor_capitals.length)];
     alert(answer);
+  }
+
+//generating an interactive keyboard
+function generateKeyboard() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+    <button id = '${letter}'
+    onClick = 'decideChoice("${letter}') >
+    ${letter}
+    </button>
+    `).join('');
+    document.getElementById("keyboard").innerHTML = buttonsHTML;
 }
 
 //showing the chosen word   
@@ -88,5 +99,5 @@ function showWord() {
         alert(won);
     }
 }
-runGame;
-showWord;
+randomWord();
+generateKeyboard();
